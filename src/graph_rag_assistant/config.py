f732f7cb759c7690acf8dc7da9c99aa7907f9739
Gellért Szalai <szalai2003@gmail.com>
2026-09-01 18:50:24 +0200

    Update requirements and enhance Apollo mission data with new documents and expanded graph entities

--- src/graph_rag_assistant/config.py
+++ src/graph_rag_assistant/config.py
@@ -5,8 +5,36 @@ DATA_DIR = PROJECT_ROOT / "data"
 DOCUMENTS_DIR = DATA_DIR / "apollo_docs"
 
 GRAPH_ENTITIES = {
-    "Person": {"Neil Armstrong", "Buzz Aldrin", "Michael Collins", "John Young", "Charles Duke", "Gene Cernan", "Harrison Schmitt"},
-    "Mission": {"Apollo 11", "Apollo 12", "Apollo 13", "Apollo 14", "Apollo 15", "Apollo 16", "Apollo 17"},
-    "Location": {"Moon", "Sea of Tranquility", "Fra Mauro", "Hadley-Apennine", "Descartes Highlands", "Taurus-Littrow"},
+    "Person": {
+        "Neil Armstrong",
+        "Buzz Aldrin",
+        "Michael Collins",
+        "John Young",
+        "Charles Duke",
+        "Gene Cernan",
+        "Harrison Schmitt",
+        "Alan Shepard",
+        "Edgar Mitchell",
+        "Charlie Conrad",
+        "Alan Bean",
+    },
+    "Mission": {
+        "Apollo 11",
+        "Apollo 12",
+        "Apollo 13",
+        "Apollo 14",
+        "Apollo 15",
+        "Apollo 16",
+        "Apollo 17",
+    },
+    "Location": {
+        "Moon",
+        "Sea of Tranquility",
+        "Fra Mauro",
+        "Hadley-Apennine",
+        "Descartes Highlands",
+        "Taurus-Littrow",
+        "Ocean of Storms",
+    },
     "Institution": {"NASA", "Kennedy Space Center", "Jet Propulsion Laboratory"},
 }
